8cd59407d4bdc195dfbfef340d364aafc9a7135f
v1adislav <your_address_email@domain.com>
2018-08-03 01:05:34 +0400

    test build

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'), // Подключаем Gulp
+    sass = require('gulp-sass'), //Подключаем Sass пакет,
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
+
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('src/scss/**/*.sass') // Берем источник
+        .pipe(sass({outputStyle: 'compressed'})) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('build/css')) // Выгружаем результата в папку build/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ["./", "./src"] 
+        }
+    });
+});
+
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('src/scss/**/*.sass', ['sass']); // Наблюдение за sass файлами
+    // Наблюдение за другими типами файлов
+});
